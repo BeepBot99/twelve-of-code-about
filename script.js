992f63565715d0d1638e88054e714eea206ae493
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2024-02-07 13:54:52 -0600

    Started testing Typewriter.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const typewriterElement = document.getElementById("typewriterDescription");
+const typewriter = new Typewriter(typewriterElement, {
+    loop: true,
+    delay: 75
+  });
+typewriter.typeString("Twelve Months of Code")
