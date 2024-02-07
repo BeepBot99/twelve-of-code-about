3cb92843b0e4decba49d392d494d9ef598fc2aeb
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2024-02-07 14:52:10 -0600

    Changed typewriter delay

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const typewriterElement = document.getElementById("typewriterDescription");
 const typewriter = new Typewriter(typewriterElement, {
     loop: true,
-    delay: 75
+    delay: 125
   });
 typewriter.pauseFor(1000)
     .typeString("Code")
