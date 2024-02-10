6dd4742d687dda0a668517562f722a2e7110cad4
BeepBot99 <Davis.Luxenberg@outlook.com>
2024-02-09 23:30:58 -0600

    Started adding animation

--- script.js
+++ script.js
@@ -19,23 +19,4 @@ const typewriter = new Typewriter(typewriterElement, {
 });
 
 // Countdown Timer until Twelve of Code is Released
-// const countdownTimer = setInterval(() => {
-//     const countDownTime = new Date("Mar 1 2024 0:0:00").getTime();
-//     const currentTime = new Date().getTime();
-//     const timeLeft = countDownTime - currentTime;
-
-//     const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-//     const hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-//     const secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-//     if (timeLeft < 0) {
-//         clearInterval(countdownTimer);
-//         document.getElementsByClassName("countdown")[0].innerHTML = "Released!";
-//     } else {
-//         document.getElementsByClassName(
-//             "countdown"
-//         )[0].innerText = `${daysLeft}d ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`;
-//     }
-// }, 1000);
-new FlipDown(1709290800, {theme: "light"}).start();
+// new FlipDown(1709290800, {theme: "light"}).start();
