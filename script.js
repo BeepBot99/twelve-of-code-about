4d78e537711c72211de04354889a8f5d3e9e4776
BeepBot99 <Davis.Luxenberg@outlook.com>
2024-02-14 07:25:52 -0600

    Started making challenge selector preview

--- script.js
+++ script.js
@@ -30,6 +30,3 @@ document.getElementById("scrollDownButton").addEventListener("click", () => {
 Array.from(document.getElementsByClassName("hide-without-javascript")).forEach(element => {
     element.style.display = "block";
 });
-function fitIframeContents(obj) {
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + "px";
-}
